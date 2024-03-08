eb6d811fd5c0e7c94dcaab5e506f15221fdbce1c
Ali Hamza <hamzamashooq492@gmail.com>
2024-03-08 21:49:17 +0500

    Create Variables

---
+++ cp-01.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string name1="Ali Hamza" ,name2="Faizan" ,name3="Usman" ,name4="Sheraz";
+	int r1=221710 ,r2=221721 ,r3=221729 ,r4=221732;
+	float gpa1=3.87 ,gpa2=3.44 ,gpa3=3.32 ,gpa4=3.67;
+	cout<<"* * * * * *Group Name* * * * * * * * * * *" <<endl
+	    <<"*                                        *" <<endl
+	    <<"*  Memder Name      Roll NO.      CGPA   *" <<endl
+	    <<"*  "<<name1<<"        "<<r1<<"        "<<gpa1<<"   *"<<endl
+	    <<"*  "<<name2<<"           "<<r2<<"        "<<gpa2<<"   *"<<endl
+	    <<"*  "<<name3<<"            "<<r3<<"        "<<gpa3<<"   *"<<endl
+	    <<"*  "<<name4<<"           "<<r4<<"        "<<gpa4<<"   *"<<endl
+	    <<"*                                        *" <<endl
+	    <<"* * * * * * * * * * * * * * * * * * * * **";
+}
