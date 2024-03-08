1cc495cb0fb50a6990f6b960c7683c086397fcec
Ali Hamza <hamzamashooq492@gmail.com>
2024-03-08 21:50:20 +0500

    Take Input

---
+++ cp-02.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout<<"Enter your name:";
+	string name;
+	cin>>name;
+	cout<<"Enter your age:";
+	int age;
+	cin>>age;
+	cout<<"Enter your semester:";
+	string sem;
+	cin>>sem;
+	cout<<"Enter your cgpa:";
+	float cgpa;
+	cin>>cgpa;
+	cout<<"* * * * * * * * * * * * * * *"<<endl
+	    <<"*                           *"<<endl
+	    <<"*    Name:   "<<name<<"            *"<<endl
+	    <<"*    Age:    "<<age<<"             *"<<endl
+	    <<"*    Sem:    "<<sem<<"            *"<<endl
+	    <<"*    Cgpa    "<<cgpa<<"           *"<<endl
+	    <<"*                           *"<<endl
+	    <<"* * * * * * * * * * * * * * *"<<endl;
+}
