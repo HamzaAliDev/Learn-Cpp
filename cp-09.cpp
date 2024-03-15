901d3b4085ba68a237d112515fc0320d249cc91a
Ali Hamza <hamzamashooq492@gmail.com>
2024-03-15 06:36:16 +0500

    Introduction with cout

---
+++ cp-09.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main (){
+	cout<<"* * * * * * Brief introduction * * * * * *" <<endl
+	    <<"*                                        *" <<endl
+	    <<"*   Name            Ali Hamza            *" <<endl
+	    <<"*   Father Name     Mashooq Ali          *" <<endl
+	    <<"*   Roll no.        22****               *" <<endl
+	    <<"*   Semester        2nd  R               *" <<endl
+	    <<"*   Inter           Fsc Pre Medical      *" <<endl
+	    <<"*   Email           ********gmail.com    *" <<endl
+	    <<"*   Cell            +92**********        *" <<endl       
+	    <<"*   City            Faisalabad           *" <<endl
+	    <<"*                                        *" <<endl
+	    <<"* * * * * * * * * * * * * * * * * * * * **";
+	    return 0;
+	
+}
